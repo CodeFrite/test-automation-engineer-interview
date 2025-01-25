5bd111dbb9dad54a9d14167e54b615aa39e6aa9c
codeFrite <codefrite@gmail.com>
2025-01-25 12:20:39 +0100

    Folder structure: adding base folder structure for a Java, Cucumber, RestAssured testing framework (made use of empty .gitkeep files to force empty folder source versioning)

---
+++ src/test/java/online/automationintesting/runners/JUnitCucumberRunner.java
@@ -0,0 +1,13 @@
+package online.automationintesting.runners;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = "online.automationintesting.stepdefinitions",
+    plugin = {"pretty", "html:src/test-reports/cucumber-report.html", "json:src/test-reports/cucumber-report.json"})
+public class JUnitCucumberRunner {
+}

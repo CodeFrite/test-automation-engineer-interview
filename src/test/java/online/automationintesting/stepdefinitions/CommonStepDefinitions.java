f60b14f18b8526db7fca7472b71ca95adeb7e72a
codeFrite <codefrite@gmail.com>
2025-01-27 22:55:10 +0100

    added some non-functional tests to the booking endpoint along with their step definitions

--- src/test/java/online/automationintesting/stepdefinitions/CommonStepDefinitions.java
+++ src/test/java/online/automationintesting/stepdefinitions/CommonStepDefinitions.java
@@ -1,20 +1,17 @@
 package online.automationintesting.stepdefinitions;
 
-import online.automationintesting.utils.TestContext;
-import static io.restassured.RestAssured.given;
-
 import java.io.File;
-
-import org.junit.Assert;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import online.automationintesting.utils.TestContext;
+import org.junit.Assert;
+import static io.restassured.RestAssured.given;
 
 public class  CommonStepDefinitions{
   private TestContext testContext;
@@ -28,7 +25,7 @@ public class  CommonStepDefinitions{
 
   /**
    * Sets the base URI for the API tests
-   * @param baseURI
+   * @param baseURI Base URI to use when sending requests
    */
   @Given("I test APIs on the base URI {string}")
   public void i_test_ap_is_on_the_base_URI(String baseURI) {
@@ -37,7 +34,7 @@ public class  CommonStepDefinitions{
 
   /**
    * Sets the base path for the API tests
-   * @param endpoint
+   * @param endpoint Endpoint to use when sending requests
    */
   @Given("I test the endpoint {string}")
   public void i_test_the_endpoint(String endpoint) {
@@ -56,6 +53,78 @@ public class  CommonStepDefinitions{
     // parse the JSON file
     ObjectMapper objectMapper = new ObjectMapper();
     JsonNode jsonNode = objectMapper.readTree(jsonFile);
+
+    // add the JSON body to the request specification
+    this.testContext.addJsonBody(jsonNode.toString());
+  }
+
+  /**
+   * Adds a cookie to the request header
+   * @param keyStore Key to store the cookie value in the test context
+   */
+  @Given("I add a cookie to the header with key {string}")
+  public void i_add_a_cookie_to_the_header_with_key(String keyStore) {
+    // retrieve the cookie value from the test context
+    String tokenValue = (String) this.testContext.getValue(keyStore);
+    this.testContext.getRequestSpecBuilder().addCookie("token", tokenValue);
+  }
+
+  /**
+   * Loads a JSON data file and stores it in the test context
+   * @param key Key to store the json object in the test context
+   * @param value Value of the JSON object to store in the test context
+   */
+  @Given("I load a json data file from {string} and store it to the test context as {string}")
+  public void i_load_a_json_data_file_from_and_store_it_to_the_test_context_as(String path, String keyStore) throws Exception{
+    // load the JSON file from the resources/data directory
+    File jsonFile = new File("src/test/resources/data/" + path + ".json");
+
+    // parse the JSON file
+    ObjectMapper objectMapper = new ObjectMapper();
+    JsonNode jsonNode = objectMapper.readTree(jsonFile);
+
+    // store the jsonNode object representation of the JSON file in the test context
+    this.testContext.storeValue(keyStore, jsonNode);
+  }
+
+  /**
+   * Updates a field in a JSON object stored in the test context
+   * @param keyStore Key to retrieve the JSON object from the test context
+   * @param fieldName Field to update in the JSON object
+   * @param newValue New value to set for the field
+   */
+  @Given("I update the {string} key in the test context with the field {string} set to {string}")
+  public void i_update_the_key_in_the_test_context_with_the_field_set_to(String keyStore, String fieldName, String newValue) {
+      // retrieve from the test context store the element to update
+      JsonNode storedElement = (JsonNode) this.testContext.getValue(keyStore);
+      System.out.println("--- i_update_the_key_in_the_test_context_with_the_field_set_to ---");
+
+      System.out.println("--- storedElement ---" + storedElement.toString());
+
+      // check if the newValue is a context variable or a litteral
+      newValue = getParameterValue(newValue);
+
+      System.out.println("--- newValue ---" + newValue);
+
+      // update the field in the stored element
+      ((ObjectNode) storedElement).put(fieldName, newValue);
+
+      // store the updated element back in the test context
+      this.testContext.storeValue(keyStore, storedElement);
+
+      System.out.println("--- i_update_the_key_in_the_test_context_with_the_field_set_to ---" + storedElement.toString());
+  }
+
+  /**
+   * Adds a JSON object stored in the test context as the request body
+   * @param keyStore Key to retrieve the JSON object from the test context
+   */
+  @Given("I use the {string} key in the test context as the request body")
+  public void i_use_the_key_in_the_test_context_as_the_request_body(String keyStore) {
+    // retrive the value from the test context
+    JsonNode jsonNode = (JsonNode) this.testContext.getValue(keyStore);
+
+    System.out.println("--- I use the {string} key in the test context as the request body ---" + jsonNode.toString());
 
     // add the JSON body to the request specification
     this.testContext.addJsonBody(jsonNode.toString());
@@ -83,8 +152,14 @@ public class  CommonStepDefinitions{
    */
   @When("I send a {string} request to {string}")
   public void i_send_a_request_to(String method, String path) {
+    // compute the path as it might be a variable
+    path = getParameterValue(path);
+
+    // send the request
     RequestSpecification requestSpecification = this.testContext.getRequestSpecification();
-    Response response = given().spec(requestSpecification).when().request(method, path);
+    Response response = given().spec(requestSpecification).when().log().all().request(method, path);
+
+    // store the response in the test context
     this.testContext.setResponse(response);
   }
 
@@ -136,4 +211,46 @@ public class  CommonStepDefinitions{
     Assert.assertNull(this.testContext.getValue(key));
   }
 
+  /**
+   * Verifies that the in context response has a non empty response body
+   */
+  @Then("I should receive an non-empty response body")
+  public void i_should_receive_an_non_empty_response_body() {
+    // extract the response body from the response
+    String responseBody = this.testContext.getResponse().getBody().asString();
+    Assert.assertNotEquals(responseBody,"");
+  }
+  // Helper methods
+
+  /**
+   * Helper method that checks if a parameter is a variable from the test context key store 
+   * by checking if they are curly braces in the expression. The rest is considered a litteral.
+   * @param parameter Parameter to check
+   */
+  private Boolean isContextVariable(String parameter) {
+    return parameter.contains("{") && parameter.contains("}");
+  }
+
+  /**
+   * Helper method that retrieves the value of a parameter from the test context if it is a variable or returns the litteral value
+   * @param parameter Parameter to retrieve the value for
+   */
+  private String getParameterValue(String parameter) {
+    System.out.println("--- getParameterValue ---" + parameter);
+    // Determine if the parameter is a litteral or a variable from the test context
+    if (isContextVariable(parameter)) {
+      // Find the position of the opening and closing curly braces
+      Integer openingCurlyBraceIndex = parameter.indexOf("{");
+      Integer closingCurlyBraceIndex = parameter.indexOf("}");
+
+      // get the value from the test context
+      String variableName = parameter.substring(openingCurlyBraceIndex+1, closingCurlyBraceIndex);
+      String variableValue = (String) this.testContext.getValue(variableName);
+
+      // add the litteral parts from left and right of the variable
+      parameter = parameter.substring(0, openingCurlyBraceIndex) + variableValue + parameter.substring(closingCurlyBraceIndex+1);
+    }
+    System.out.println("--- getParameterValue ---" + parameter);
+    return parameter;
+  }
 }
---
+++ src/test/resources/features/Booking.NonFunctionalTesting.feature
@@ -0,0 +1,52 @@
+Feature: Non-Functional Testing of the `Booking` endpoint
+
+  Background: Set the base URL and path for the booking endpoint
+    Given I test APIs on the base URI "https://automationintesting.online"
+      And I test the endpoint "/booking"
+
+  # getBooking operation
+  Scenario: NF.001: (auth) Get an existing booking and observe 200 (OK) + booking object in the response body
+    Given I am authenticated
+      And I add a cookie to the header with key "token"
+      And I have a new room available for booking with id "new-room"
+      And I make a booking "new-booking" for the room with id "new-room"
+     When I send a "GET" request to "/{new-room}"
+     Then the response should have a status code 200
+      And I should receive an non-empty response body
+
+
+  #Scenario: NF.002: (no auth) Get an existing booking and observe 403 (Forbidden) + error message "Forbidden"
+  #Scenario: NF.003: (no auth) Get a non-existing booking and observe 403 (Forbidden) + error message "Forbidden"
+  #Scenario: NF.004: (auth) Get a non-existing booking and observe 404 (Not Found) + error message "Not Found"
+  #Scenario: NF.005: (auth) No booking id in path and observe equivalent to operation `getBookings`
+  #Scenario: NF.006: (auth) Get a booking with id 0 and observe 400 (Bad Request) + error message "bookingid out of range"
+  #Scenario: NF.007: (auth) Get a booking with id -1 and observe 400 (Bad Request) + error message "bookingid out of range"
+  #Scenario: NF.008: (auth) Get a booking with id abc and observe 400 (Bad Request) + error message "bookingid wrong data type: got string should be int32"
+  #Scenario: NF.009: (auth) Get a booking with id 1000000000 and observe 400 (Bad Request) + error message "bookingid out of range"
+
+  # updateBooking operation
+  #Scenario: NF.101: (auth) Update an existing booking with valid data and observe 200 (OK) + booking changed with new data
+  #Scenario: NF.102: (no auth) Update an existing booking with valid data and observe 403 (Forbidden)
+  #Scenario: NF.103: (auth) Update an existing booking with invalid data and observe 400 (Bad Request)
+
+  # deleteBooking operation
+  #Scenario: NF.201: (auth) Delete an existing booking and observe 202 (Accepted)
+  #Scenario: NF.202: (no auth) Delete an existing booking and observe 403 (Forbidden)
+  #Scenario: NF.203: (auth) Delete a non-existing booking and observe 404 (Not Found)
+
+  # createBooking operation
+  #Scenario: NF.301: (auth) Create a booking with valid data and observe 201 (Created) + booking object in the response body
+  #Scenario: NF.302: (no auth) Create a booking with valid data and observe 403 (Forbidden)
+  #Scenario: NF.303: (auth) Create a booking for a room already booked for that single date and observe 409 (Conflict)
+  #Scenario: NF.304: (auth) Create a booking for a room already booked for that period and observe 409 (Conflict)
+  #Scenario: NF.305: (auth) Create a booking with invalid data and observe 400 (Bad Request)
+  #Scenario: NF.306: (auth) Create a booking for a room already booked for that period with invalid data and observe 400 (Bad Request)
+
+  # getBookings operation
+  #Scenario: NF.401: (auth) Get all bookings for an existing room and observe 200 (OK) + list of `Booking` objects in the response body
+  #Scenario: NF.402: (no auth) Get all bookings for an existing room and observe 403 (Forbidden)
+  #Scenario: NF.403: (auth) Get all bookings for a non-existing room and observe 404 (Not Found)
+
+  # getSummaries operation
+  #Scenario: NF.501: (auth) Get summaries of all bookings for a room that has been booked and observe 200 (OK) + list of `BookingDates` objects in the response body
+  #Scenario: NF.502: (auth) Get summaries of all bookings for a room that has not been booked and observe 200 (OK) + empty list

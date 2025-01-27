5e98c883f429157e90baf5649bd201d10f0d281a
codeFrite <codefrite@gmail.com>
2025-01-27 22:52:13 +0100

    RoomStepDefinitions.java: added a quick step to add a booking for a given room

--- src/test/java/online/automationintesting/stepdefinitions/RoomStepDefinitions.java
+++ src/test/java/online/automationintesting/stepdefinitions/RoomStepDefinitions.java
@@ -1,19 +1,20 @@
 package online.automationintesting.stepdefinitions;
 
 import static io.restassured.RestAssured.given;
-
-import org.junit.Assert;
-
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import online.automationintesting.pojo.Booking;
+import online.automationintesting.pojo.BookingDates;
 import online.automationintesting.pojo.Room;
 import online.automationintesting.utils.TestContext;
+import org.junit.Assert;
 
 public class RoomStepDefinitions {
   
@@ -64,6 +65,65 @@ public class RoomStepDefinitions {
   }
 
   /**
+   * Quick Step to create a new booking for a given room and store the booking id in the test context store
+   * @param bookingKey The key to store the booking id in the test context store
+   * @param roomKey The key to retrieve the room id from the test context store
+   * @throws Exception
+   */
+  @Given("I make a booking {string} for the room with id {string}")
+  public void i_make_a_booking_for_the_room_with_id(String bookingKey, String roomKey) throws Exception {
+    System.out.println(">>>>>>> roomKey/value: " + roomKey + "/" + this.testContext.getValue(roomKey));
+    System.out.println(">>>>>>> token: " + this.testContext.getValue("token"));
+    System.out.println(">>>>>>> bookingKey: " + bookingKey);
+    // retrieve the room id from the test context store
+    String roomId = this.testContext.getValue(roomKey).toString();
+
+    // Instantiate a new Room object
+    Booking booking = new Booking(
+      0,
+      Integer.parseInt(roomId),
+      "John",
+      "Doe",
+      true,
+      "john.doe@gmail.com",
+      "+32/123456789",
+      new BookingDates(
+        LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+        LocalDate.now().plusDays(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+      )
+      );
+  
+    // convert the Room object to a JSON string
+    ObjectMapper objectMapper = new ObjectMapper();
+    String jsonBody = objectMapper.writeValueAsString(booking);
+
+    // Instantiate a new RequestSpecBuilder object to not pollute the Test Context
+    RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+    
+    // add all necessary headers, the auth token and the JSON body to the RequestSpecBuilder object
+    RequestSpecification requestSpecification = requestSpecBuilder
+    .addHeader("Cache-Control", "no-cache")
+    .addHeader("Host", "automationintesting.online")
+    .setBaseUri("https://automationintesting.online/booking/")
+    .addCookie("token", this.testContext.getValue("token").toString())
+    .addHeader("Content-Type", "application/json")
+    .setBody(jsonBody)
+    .build();
+
+    // call the createBooking endpoint to create the new booking
+    Response response = given().spec(requestSpecification).when().post();
+
+    // check status code
+    response.then().statusCode(201);
+
+    // retrieve the booking id from the response body
+    Integer bookingId = response.then().extract().jsonPath().get("bookingid");
+
+    // store the booking id into the test context store
+    this.testContext.storeValue(bookingKey, bookingId);
+  }
+
+  /**
    * Quick Step to delete a room by its id
    * @param roomKey The key to retrieve the room id from the test context store
    */
@@ -92,5 +152,4 @@ public class RoomStepDefinitions {
     // delete the room id from the test context store
     Assert.assertNotNull(this.testContext.removeKeyValue(roomKey));
   }
-
 }

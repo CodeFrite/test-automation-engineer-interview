2c93918eb69fb1dffc5e06b4b45fe91fc963e866
codeFrite <codefrite@gmail.com>
2025-01-27 18:11:31 +0100

    Helper.java: added static method setField to set the value of the member of an object using directly the member name using the reflect Java library

--- src/main/java/online/automationintesting/utils/Helper.java
+++ src/main/java/online/automationintesting/utils/Helper.java
@@ -1,5 +1,6 @@
 package online.automationintesting.utils;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,12 +8,12 @@ import java.util.Random;
 
 public class Helper {
 
-    /**
-     * Generates a list of x random distinct values from 0 to y-1
-     * @param x Number of values to generate
-     * @param y Maximum value (exclusive)
-     * @return List of x random distinct values
-     */
+  /**
+   * Generates a list of x random distinct values from 0 to y-1
+   * @param x Number of values to generate
+   * @param y Maximum value (exclusive)
+   * @return List of x random distinct values
+   */
   public static Integer[] getRandomDistinctValues(int x, int y) {
         // Generate a list of values from 0 to y-1
         List<Integer> values = new ArrayList<>();
@@ -26,5 +27,19 @@ public class Helper {
         // Return the first x elements
         return values.subList(0, x).toArray(Integer[]::new);
     }
+
+ 
+  /**
+   * Sets the value of a field in an object based on the field name
+   * @param obj Object to set the field value in
+   * @param fieldName Name of the field to set
+   * @param value Value to set the field to
+   * @throws Exception
+   */
+  public static void setField(Object obj, String fieldName, Object value) throws Exception {
+    Field field = obj.getClass().getDeclaredField(fieldName);
+    field.setAccessible(true); // Bypass private access
+    field.set(obj, value);
+  }
   
 }
